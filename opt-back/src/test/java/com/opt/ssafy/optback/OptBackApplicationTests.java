751d3d10d3f43fcc70f276e08fb517b171131618
SSAFY <swoolee97@ajou.ac.kr>
2025-01-24 11:20:05 +0900

    init: 프로젝트 생성

---
+++ opt-back/src/test/java/com/opt/ssafy/optback/OptBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opt.ssafy.optback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

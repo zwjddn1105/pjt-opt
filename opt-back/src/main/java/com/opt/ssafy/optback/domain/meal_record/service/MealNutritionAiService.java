9e93b527ad9fe168f79e043d506a74f1d1e8f4bb
JIEUN <jsilver0127@gmail.com>
2025-02-07 23:38:56 +0900

    feat: AI리포트 기능 추가 및 AI관련 코드 리팩토링

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/meal_record/service/MealNutritionAiService.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/meal_record/service/MealNutritionAiService.java
@@ -17,11 +17,9 @@ public class MealNutritionAiService {
 
     // AI로 이미지 분석
     public MealNutritionDto analyzeMealImage(String imagePath) {
-        String prompt = String.format(
-                "다음 식단의 이미지 URL을 참고하여 이 식단의 영양 정보를 알려주십시오. "
-                        + "반드시 아래와 같은 JSON 형식으로 답변해야 합니다.\n"
-                        + "{\"calorie\": <총 칼로리>, \"protein\": <단백질>, \"carb\": <탄수화물>, \"fat\": <지방>}\n"
-                        + "식단이미지 URL: %s", imagePath);
+        String prompt = "다음 식단의 이미지 URL을 참고하여 이 식단의 영양 정보를 알려주십시오. 반드시 아래와 같은 JSON 형식으로 답변해야 합니다.\n"
+                + "{\"calorie\": <총 칼로리>, \"protein\": <단백질>, \"carb\": <탄수화물>, \"fat\": <지방>}\n"
+                + "식단이미지 URL: " + imagePath;
 
         Object response = gptService.requestGPT(prompt);
 

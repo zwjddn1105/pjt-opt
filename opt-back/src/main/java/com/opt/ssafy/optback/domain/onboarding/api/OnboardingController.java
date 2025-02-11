fd3d4809b140b6c9679d16f1fc4399161f28e422
DduMandoo <ojs9551@naver.com>
2025-02-11 17:21:09 +0900

    fix: 컨트롤러 인자 입력 방식 수정

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/onboarding/api/OnboardingController.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/onboarding/api/OnboardingController.java
@@ -3,7 +3,6 @@ package com.opt.ssafy.optback.domain.onboarding.api;
 import com.opt.ssafy.optback.domain.onboarding.application.OnboardingService;
 import com.opt.ssafy.optback.domain.onboarding.dto.OnboardingRequest;
 import com.opt.ssafy.optback.global.dto.SuccessResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +15,7 @@ public class OnboardingController {
     private final OnboardingService onboardingService;
 
     @PostMapping
-    public ResponseEntity<SuccessResponse> onboard(
-            @RequestParam String nickname,
-            @RequestParam List<Integer> interestIds) {
-
-        OnboardingRequest request = new OnboardingRequest(nickname, interestIds);
+    public ResponseEntity<SuccessResponse> onboard(@RequestBody OnboardingRequest request) {
         onboardingService.onboardMember(request);
 
         return ResponseEntity.ok(SuccessResponse.builder()

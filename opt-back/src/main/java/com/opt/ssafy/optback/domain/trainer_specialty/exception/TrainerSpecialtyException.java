e9fec769a182207a97daa822955785896cd0a72d
SSAFY <swoolee97@ajou.ac.kr>
2025-02-13 14:24:45 +0900

    delete: 트레이너 전문분야 기능 삭제

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/trainer_specialty/exception/TrainerSpecialtyException.java
+++
@@ -1,7 +0,0 @@
-package com.opt.ssafy.optback.domain.trainer_specialty.exception;
-
-public class TrainerSpecialtyException extends RuntimeException {
-    public TrainerSpecialtyException(String message) {
-        super(message);
-    }
-}

cfb88abc0554e45e2900ab048a70027bbaf3aec5
SSAFY <swoolee97@ajou.ac.kr>
2025-02-13 14:21:13 +0900

    feat: 프로필 불러오는 기능

---
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/profile/ProfileService.java
@@ -0,0 +1,38 @@
+package com.opt.ssafy.optback.domain.profile;
+
+import com.opt.ssafy.optback.domain.auth.application.UserDetailsServiceImpl;
+import com.opt.ssafy.optback.domain.badge.entity.Badge;
+import com.opt.ssafy.optback.domain.badge.repository.BadgeRepository;
+import com.opt.ssafy.optback.domain.member.entity.Member;
+import com.opt.ssafy.optback.domain.member.exception.MemberNotFoundException;
+import com.opt.ssafy.optback.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final MemberRepository memberRepository;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final BadgeRepository badgeRepository;
+
+    public ProfileResponse getMyProfile() {
+        Member member = userDetailsService.getMemberByContextHolder();
+        return getProfileResponse(member);
+    }
+
+    public ProfileResponse getProfile(Integer memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        return getProfileResponse(member);
+    }
+
+    public ProfileResponse getProfileResponse(Member member) {
+        Badge mainBadge = badgeRepository.findById(member.getMainBadgeId()).orElse(null);
+        if (member.isTrainer()) {
+            return TrainerProfileResponse.from(member, mainBadge);
+        }
+        return MemberProfileResponse.from(member, mainBadge);
+    }
+
+}
---
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/profile/TrainerProfileResponse.java
@@ -0,0 +1,24 @@
+package com.opt.ssafy.optback.domain.profile;
+
+import com.opt.ssafy.optback.domain.badge.dto.BadgeResponse;
+import com.opt.ssafy.optback.domain.badge.entity.Badge;
+import com.opt.ssafy.optback.domain.member.entity.Member;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class TrainerProfileResponse extends ProfileResponse {
+    private String name;
+
+    public static TrainerProfileResponse from(Member member, Badge mainBadge) {
+        return TrainerProfileResponse.builder()
+                .id(member.getId())
+                .role(member.getRole().name())
+                .name(member.getName())
+                .mainBadge(new BadgeResponse(mainBadge))
+                .nickname(member.getNickname())
+                .imagePath(member.getImagePath())
+                .build();
+    }
+}

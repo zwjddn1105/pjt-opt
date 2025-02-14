f3edd9422f7aec49161b389efd20dad19847b58c
CheonWoo <cjsgusdn14@naver.com>
2025-02-14 12:13:47 +0900

    feat: comment processing at WebScoketSecurityConfig.java

--- opt-back/src/main/java/com/opt/ssafy/optback/config/WebSocketSecurityConfig.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/config/WebSocketSecurityConfig.java
@@ -1,24 +1,24 @@
-package com.opt.ssafy.optback.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-
-@Configuration
-public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        messages
-                .nullDestMatcher().permitAll()
-                .simpDestMatchers("/app/**").authenticated()
-                .simpDestMatchers("/user/**").authenticated()
-                .simpSubscribeDestMatchers("/topic/**").authenticated()
-                .anyMessage().denyAll();
-    }
-
-    @Override
-    protected boolean sameOriginDisabled() {
-        return true;
-    } // CORS 보안 정책 비활성화
-}
+//package com.opt.ssafy.optback.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+//import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//        messages
+//                .nullDestMatcher().permitAll()
+//                .simpDestMatchers("/app/**").authenticated()
+//                .simpDestMatchers("/user/**").authenticated()
+//                .simpSubscribeDestMatchers("/topic/**").authenticated()
+//                .anyMessage().denyAll();
+//    }
+//
+//    @Override
+//    protected boolean sameOriginDisabled() {
+//        return true;
+//    } // CORS 보안 정책 비활성화
+//}

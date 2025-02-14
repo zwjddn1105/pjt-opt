4a89080f4404caa6e2faf7e5b88c0c9425eceb0d
이승우 <swoolee97@ajou.ac.kr>
2025-02-15 01:17:47 +0900

    fix: 인가 필요 없는 요청 필터링되는 오류 수정

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/auth/filter/JwtAuthenticationFilter.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/auth/filter/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.opt.ssafy.optback.domain.auth.filter;
 
 import com.opt.ssafy.optback.domain.auth.application.JwtProvider;
-import com.opt.ssafy.optback.domain.auth.application.TokenBlacklistService;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
@@ -19,30 +18,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
-    private final TokenBlacklistService tokenBlacklistService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        System.out.println("🐿️ JWT 필터 실행 요청 URI: " + request.getRequestURI());
-        if (request.getRequestURI().startsWith("/auth")) {
-            System.out.println("🐿️ /auth 요청 JWT 필터 통과");
-            filterChain.doFilter(request, response);
-            return;
-        }
-        if (request.getRequestURI().startsWith("/ws-chat")) {
-            System.out.println("🐿️ /ws-chat 요청 JWT 필터 통과");
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         try {
             String token = resolveToken(request);
-            // 로그아웃된 토큰인지 확인
-            if (token != null & tokenBlacklistService.isBlackListed(token)) {
-                throw new SecurityException("로그아웃된 토큰입니다");
-            }
             // 2. validateToken 으로 토큰 유효성 검사
             if (token != null && jwtProvider.validateToken(token)) {
                 // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext 에 저장

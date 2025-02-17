dfd0d9c10d80e9e61f18b3f9bf8b725009634aaa
SSAFY <swoolee97@ajou.ac.kr>
2025-02-17 15:30:46 +0900

    fix: 카카오 프로필 사진 불러오지 못하는 에러 해결

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/auth/dto/KakaoMemberInfo.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/auth/dto/KakaoMemberInfo.java
@@ -4,40 +4,72 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class KakaoMemberInfo {
+
     @JsonProperty("id")
-    private String id;
+    private Long id;
+
+    @JsonProperty("connected_at")
+    private String connectedAt;
 
     @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
     public String getEmail() {
-        return kakaoAccount.getEmail();
+        return kakaoAccount != null ? kakaoAccount.getEmail() : null;
     }
 
     public String getProfileImageUrl() {
         return kakaoAccount.getProfile().getProfileImageUrl();
     }
 
+    public String getNickname() {
+        return (kakaoAccount != null && kakaoAccount.getProfile() != null)
+                ? kakaoAccount.getProfile().getNickname()
+                : null;
+    }
+
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @ToString
     public static class KakaoAccount {
         private String email;
+
+        @JsonProperty("profile_nickname_needs_agreement")
+        private Boolean profileNicknameNeedsAgreement;
+
+        @JsonProperty("profile_image_needs_agreement")
+        private Boolean profileImageNeedsAgreement;
 
         @JsonProperty("profile")
         private Profile profile;
     }
 
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @ToString
     public static class Profile {
+        @JsonProperty("nickname")
+        private String nickname;
+
+        @JsonProperty("thumbnail_image_url")
+        private String thumbnailImageUrl;
 
         @JsonProperty("profile_image_url")
         private String profileImageUrl;
-    }
 
+        @JsonProperty("is_default_image")
+        private Boolean isDefaultImage;
+
+        @JsonProperty("is_default_nickname")
+        private Boolean isDefaultNickname;
+    }
 }

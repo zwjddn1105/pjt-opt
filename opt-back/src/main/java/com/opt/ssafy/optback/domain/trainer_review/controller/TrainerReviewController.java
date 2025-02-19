c3fb747760fb4e865aa059c81e4ca75d07b15d63
SSAFY <swoolee97@ajou.ac.kr>
2025-02-19 14:56:54 +0900

    feat: 트레이너 리뷰 이미지 삭제

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/trainer_review/controller/TrainerReviewController.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/trainer_review/controller/TrainerReviewController.java
@@ -6,20 +6,16 @@ import com.opt.ssafy.optback.domain.trainer_review.dto.TrainerReviewSummaryRespo
 import com.opt.ssafy.optback.domain.trainer_review.entity.TrainerReview;
 import com.opt.ssafy.optback.domain.trainer_review.service.TrainerReviewImageService;
 import com.opt.ssafy.optback.domain.trainer_review.service.TrainerReviewService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,16 +26,10 @@ public class TrainerReviewController {
     private final TrainerReviewImageService trainerReviewImageService;
 
     // 리뷰(텍스트+이미지) 추가
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<TrainerReviewResponse> addTrainerReview(
-            @RequestPart("review") TrainerReviewRequest reviewRequestDto,
-            @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+    @PostMapping
+    public ResponseEntity<TrainerReviewResponse> addTrainerReview(TrainerReviewRequest reviewRequestDto) {
         // 리뷰 텍스트 저장
         TrainerReview savedTrainerReview = trainerReviewService.saveReviewText(reviewRequestDto);
-        // 리뷰 이미지 저장
-        if (images != null && !images.isEmpty()) {
-            trainerReviewImageService.saveReviewImages(savedTrainerReview, images);
-        }
         return ResponseEntity.ok(new TrainerReviewResponse(savedTrainerReview));
 
     }

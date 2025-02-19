79df39d8ed99f0f522f736d8707a9d7b6f15397a
DduMandoo <ojs9551@naver.com>
2025-02-19 14:36:46 +0900

    fix: 응답 키 변경 v2

--- opt-back/src/main/java/com/opt/ssafy/optback/domain/profile/ProfileResponse.java
+++ opt-back/src/main/java/com/opt/ssafy/optback/domain/profile/ProfileResponse.java
@@ -1,6 +1,7 @@
 package com.opt.ssafy.optback.domain.profile;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.opt.ssafy.optback.domain.badge.dto.BadgeResponse;
 import com.opt.ssafy.optback.domain.member.entity.Interest;
 import java.util.List;
@@ -17,6 +18,12 @@ public class ProfileResponse {
     private BadgeResponse mainBadge;
     private List<Interest> interests;
 
-    @JsonProperty("isFollow")
+    @JsonIgnore // 필드 직렬화를 무시
     private boolean isFollow;
+
+    @JsonGetter("isFollow") // getter에서 "isFollow"라는 이름으로 직렬화
+    public boolean isFollow() {
+        return isFollow;
+    }
+
 }
